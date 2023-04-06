f3abd1b6fa3dbf9695d8784da800dddc3b4c931d
Albina <drozdowaalbina@yandex.ru>
2023-04-06 20:41:17 +0300

    дз Семинар 9

---
+++ task66/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, 
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+Console.WriteLine("Вводим первое число:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вводим второе число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int numDegree = NumberDegree(m, n);
+Console.WriteLine($"Ответ: {numDegree}");
+
+int NumberDegree(int num1, int num2)
+{
+
+    if (num1 == num2) return num2;
+    return num2 + NumberDegree(num1, num2 - 1);
+}
---
+++ task68/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+
+
+Console.WriteLine("Вводим первое положительное число:");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вводим второе положительное число:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int result = Akker(m, n);
+Console.WriteLine($"Ответ: {result}");
+
+
+
+int Akker(int m, int n)
+{
+    {
+        if (m == 0) 
+        return n + 1;
+        else if (n == 0) 
+        return Akker(m - 1, 1);
+        else return Akker(m - 1, Akker(m, n - 1));
+    }
+}
